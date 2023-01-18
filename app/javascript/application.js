fc498953c6f1e6abb124d4953006001ad9b35a11
Ubuntu <ubuntu@ip-172-31-6-109.ec2.internal>
2023-01-18 09:10:56 +0000

    Added javascript for phone menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+let menuOpen = false;
+const menuButton = document.querySelector("#mobile_burger");
+const menuBar = document.querySelector("#mobile_menu");
+
+function menuToggle() {
+    menuOpen?menuBar.style.display("none"):menuBar.style.display ="flex";
+    
+}
+
+menuButton.addEventListener("touchStart", menuToggle());
+
+
+
+
