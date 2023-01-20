a325b8a2c06ca61fa117e4cf42ac1dba9701c249
Ubuntu <ubuntu@ip-172-31-6-109.ec2.internal>
2023-01-20 09:26:48 +0000

    Added validation to groups, and wrap to text on cards

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
 class Group < ApplicationRecord
+    
+    validates :name, presence: true
+    validates :name, length: { maximum: 50 }
+    validates :location, presence: true
+    validates :location, length: {maximum: 500 }
+    
 end
