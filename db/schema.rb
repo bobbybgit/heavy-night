9d083f551d469888fbd086b2663937eab9839d84
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-01-21 20:18:33 +0000

    Moved to codespaces updated ruby version etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_202635) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_200511) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "location"
